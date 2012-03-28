65d8816734d36eebbd8f05f97f80a6fa970150e8
Kyle Langille <langille.kyle@gmail.com>
2012-03-28 20:42:57 -0300

    Starting Xcode project

---
+++ Snowboard/Snowboard/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Snowboard
+//
+//  Created by Kyle Langille on 12-03-28.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
