58470a338666607530f79c07e5566f987ed95d85
Kyle Langille <langille.kyle@gmail.com>
2012-03-28 23:31:10 -0300

    Game development started

--- Snowboard/Snowboard/HelloWorldLayer.h
+++ Snowboard/Snowboard/HelloWorldLayer.h
@@ -10,9 +10,35 @@
 // When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
 
+typedef enum {
+    kEndReasonWin,
+    kEndReasonLose
+} EndReason;
+
 // HelloWorldLayer
 @interface HelloWorldLayer : CCLayer
 {
+    CCSpriteBatchNode *_batchNode;
+    CCSprite *_ship;
+    
+    CCParallaxNode *_backgroundNode;
+    CCSprite *_spacedust1;
+    CCSprite *_spacedust2;
+    CCSprite *_planetsunrise;
+    CCSprite *_galaxy;
+    CCSprite *_spacialanomaly;
+    CCSprite *_spacialanomaly2;
+    float _shipPointsPerSecY;
+    float _shipPointsPerSecZ;
+    
+    CCArray *_asteroids;
+    int _nextAsteroid;
+    double _nextAsteroidSpawn;
+    
+    int _lives;
+    
+    double _gameOverTime;
+    bool _gameOver;
 }
 
 // returns a CCScene that contains the HelloWorldLayer as the only child
--- Snowboard/Snowboard/HelloWorldLayer.m
+++ Snowboard/Snowboard/HelloWorldLayer.m
@@ -9,6 +9,9 @@
 
 // Import the interfaces
 #import "HelloWorldLayer.h"
+#import "CCParallaxNode-Extras.h"
+
+#define kNumAsteroids 4
 
 // HelloWorldLayer implementation
 @implementation HelloWorldLayer
@@ -31,23 +34,239 @@
 // on "init" you need to initialize your instance
 -(id) init
 {
-	// always call "super" init
-	// Apple recommends to re-assign "self" with the "super" return value
-	if( (self=[super init])) {
-		
-		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
+    if( (self=[super init])) {
+        
+        _batchNode = [CCSpriteBatchNode batchNodeWithFile:@"Sprites.pvr.ccz"]; // 1
+        [self addChild:_batchNode]; // 2
+        [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"Sprites.plist"]; // 3
+        
+        _ship = [CCSprite spriteWithSpriteFrameName:@"SpaceFlier_sm_1.png"];  // 4
+        CGSize winSize = [CCDirector sharedDirector].winSize; // 5
+        _ship.position = ccp(winSize.width * 0.1, winSize.height * 0.5); // 6
+        [_batchNode addChild:_ship z:1]; // 7
+        
+        // 1) Create the CCParallaxNode
+        _backgroundNode = [CCParallaxNode node];
+        [self addChild:_backgroundNode z:-1];
+        
+        // 2) Create the sprites we'll add to the CCParallaxNode
+        _spacedust1 = [CCSprite spriteWithFile:@"bg_front_spacedust.png"];
+        _spacedust2 = [CCSprite spriteWithFile:@"bg_front_spacedust.png"];
+        _planetsunrise = [CCSprite spriteWithFile:@"bg_planetsunrise.png"];
+        _galaxy = [CCSprite spriteWithFile:@"bg_galaxy.png"];
+        _spacialanomaly = [CCSprite spriteWithFile:@"bg_spacialanomaly.png"];
+        _spacialanomaly2 = [CCSprite spriteWithFile:@"bg_spacialanomaly2.png"];
+        
+        // 3) Determine relative movement speeds for space dust and background
+        CGPoint dustSpeed = ccp(0.1, 0.1);
+        CGPoint bgSpeed = ccp(0.05, 0.05);
+        
+        // 4) Add children to CCParallaxNode
+        [_backgroundNode addChild:_spacedust1 z:0 parallaxRatio:dustSpeed positionOffset:ccp(0,winSize.height/2)];
+        [_backgroundNode addChild:_spacedust2 z:0 parallaxRatio:dustSpeed positionOffset:ccp(_spacedust1.contentSize.width,winSize.height/2)];        
+        [_backgroundNode addChild:_galaxy z:-1 parallaxRatio:bgSpeed positionOffset:ccp(0,winSize.height * 0.7)];
+        [_backgroundNode addChild:_planetsunrise z:-1 parallaxRatio:bgSpeed positionOffset:ccp(600,winSize.height * 0)];        
+        [_backgroundNode addChild:_spacialanomaly z:-1 parallaxRatio:bgSpeed positionOffset:ccp(900,winSize.height * 0.3)];        
+        [_backgroundNode addChild:_spacialanomaly2 z:-1 parallaxRatio:bgSpeed positionOffset:ccp(1500,winSize.height * 0.9)];
+        
+        NSArray *starsArray = [NSArray arrayWithObjects:@"Stars1.plist", @"Stars2.plist", @"Stars3.plist", nil];
+        for(NSString *stars in starsArray) {        
+            CCParticleSystemQuad *starsEffect = [CCParticleSystemQuad particleWithFile:stars];        
+            [self addChild:starsEffect z:1];
+        }
+        
+        self.isAccelerometerEnabled = YES;
+        
+        _asteroids = [[CCArray alloc] initWithCapacity:kNumAsteroids];
+        for(int i = 0; i < kNumAsteroids; ++i) {
+            CCSprite *asteroid = [CCSprite spriteWithFile:@"dead-tree.png"];
+            asteroid.visible = NO;
+            [self addChild:asteroid];
+            [_asteroids addObject:asteroid];
+        }
+        
+        _lives = 3;
+        double curTime = CACurrentMediaTime();
+        _gameOverTime = curTime + 30.0;
+        
+        [self scheduleUpdate];
+    }
+    return self;
+}
 
-		// ask director the the window size
-		CGSize size = [[CCDirector sharedDirector] winSize];
-	
-		// position the label on the center of the screen
-		label.position =  ccp( size.width /2 , size.height/2 );
-		
-		// add the label as a child to this Layer
-		[self addChild: label];
-	}
-	return self;
+- (float)randomValueBetween:(float)low andValue:(float)high {
+    return (((float) arc4random() / 0xFFFFFFFFu) * (high - low)) + low;
+}
+
+- (void)update:(ccTime)dt {
+    CGPoint backgroundScrollVel = ccp(-1000, 0);
+    _backgroundNode.position = ccpAdd(_backgroundNode.position, ccpMult(backgroundScrollVel, dt));
+    
+    NSArray *spaceDusts = [NSArray arrayWithObjects:_spacedust1, _spacedust2, nil];
+    for (CCSprite *spaceDust in spaceDusts) {
+        if ([_backgroundNode convertToWorldSpace:spaceDust.position].x < -spaceDust.contentSize.width) {
+            [_backgroundNode incrementOffset:ccp(2*spaceDust.contentSize.width,0) forChild:spaceDust];
+        }
+    }
+    
+    NSArray *backgrounds = [NSArray arrayWithObjects:_planetsunrise, _galaxy, _spacialanomaly, _spacialanomaly2, nil];
+    for (CCSprite *background in backgrounds) {
+        if ([_backgroundNode convertToWorldSpace:background.position].x < -background.contentSize.width) {
+            [_backgroundNode incrementOffset:ccp(2000,0) forChild:background];
+        }
+    }
+    
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    float maxY = winSize.height - _ship.contentSize.height/2;
+    float minY = _ship.contentSize.height/2;
+    
+    float newY = _ship.position.y + (_shipPointsPerSecY * dt);
+    newY = MIN(MAX(newY, minY), maxY);
+    
+    float maxX = winSize.width - _ship.contentSize.width/2;
+    float minX = _ship.contentSize.width/2;
+    
+    float newX = _ship.position.x + (_shipPointsPerSecZ * dt);
+    newX = MIN(MAX(newX, minX), maxX);
+    //_ship.position = ccp(newX, newY);
+    _ship.position = ccp(_ship.position.x, newY);
+    
+    double curTime = CACurrentMediaTime();
+    if (curTime > _nextAsteroidSpawn) {
+        
+        float randSecs = [self randomValueBetween:2 andValue:2.5];
+        _nextAsteroidSpawn = randSecs + curTime;
+        
+        float randY = [self randomValueBetween:0.0 andValue:winSize.height];
+        float randDuration = 10;//[self randomValueBetween:2.0 andValue:10.0];
+        
+        CCSprite *asteroid = [_asteroids objectAtIndex:_nextAsteroid];
+        _nextAsteroid++;
+        if (_nextAsteroid >= _asteroids.count) _nextAsteroid = 0;
+        
+        [asteroid stopAllActions];
+        asteroid.position = ccp(winSize.width+asteroid.contentSize.width/2, randY);
+        asteroid.visible = YES;
+        [asteroid runAction:[CCSequence actions:
+                             [CCMoveBy actionWithDuration:randDuration position:ccp(-winSize.width-asteroid.contentSize.width, 0)],
+                             [CCCallFuncN actionWithTarget:self selector:@selector(setInvisible:)],
+                             nil]];
+    }
+    
+    for (CCSprite *asteroid in _asteroids) {        
+        if (!asteroid.visible) continue;
+        
+        /*for (CCSprite *shipLaser in _shipLasers) {                        
+            if (!shipLaser.visible) continue;
+            
+            if (CGRectIntersectsRect(shipLaser.boundingBox, asteroid.boundingBox)) {                
+                shipLaser.visible = NO;
+                asteroid.visible = NO;                
+                continue;
+            }
+        }*/
+        
+        if (CGRectIntersectsRect(_ship.boundingBox, asteroid.boundingBox)) {
+            asteroid.visible = NO;
+            [_ship runAction:[CCBlink actionWithDuration:1.0 blinks:9]];            
+            _lives--;
+        }
+    }
+    
+    if (_lives <= 0) {
+        [_ship stopAllActions];
+        _ship.visible = FALSE;
+        [self endScene:kEndReasonLose];
+    } else if (curTime >= _gameOverTime) {
+        [self endScene:kEndReasonWin];
+    }
+}
+
+- (void)setInvisible:(CCNode *)node {
+    node.visible = NO;
+}
+
+- (void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration {
+    
+    #define kFilteringFactor .5
+    #define kRestAccelX 0
+    #define kRestAccelZ -0.6
+    #define kShipMaxPointsPerSecWidth (winSize.width*10)
+    #define kShipMaxPointsPerSecHeight (winSize.height*0.5)
+    #define kMaxDiffX 0.9
+    #define kMaxDiffZ 0.3
+    
+    UIAccelerationValue rollingX, rollingY, rollingZ;
+    
+    rollingX = (acceleration.x * kFilteringFactor) + (rollingX * (1.0 - kFilteringFactor));    
+    rollingY = (acceleration.y * kFilteringFactor) + (rollingY * (1.0 - kFilteringFactor));    
+    rollingZ = (acceleration.z * kFilteringFactor) + (rollingZ * (1.0 - kFilteringFactor));
+    
+    float accelX = acceleration.x - rollingX;
+    float accelY = acceleration.y - rollingY;
+    float accelZ = acceleration.z - rollingZ;
+    
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    
+    float accelDiff = accelX;
+    float accelFraction = accelDiff / kMaxDiffX;
+    float pointsPerSec = kShipMaxPointsPerSecWidth * accelX;
+    
+    float accelDiff2 = accelZ - kRestAccelZ;
+    float accelFraction2 = accelDiff2 / kMaxDiffZ;
+    float pointsPerSec2 = kShipMaxPointsPerSecHeight * accelFraction2;
+    
+    _shipPointsPerSecY = pointsPerSec;
+    _shipPointsPerSecZ = pointsPerSec2;
+    
+}
+
+- (void)restartTapped:(id)sender {
+    [[CCDirector sharedDirector] replaceScene:[CCTransitionZoomFlipX transitionWithDuration:0.5 scene:[HelloWorldLayer scene]]];   
+}
+
+- (void)endScene:(EndReason)endReason {
+    
+    if (_gameOver) return;
+    _gameOver = true;
+    
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    
+    NSString *message;
+    if (endReason == kEndReasonWin) {
+        message = @"You win!";
+    } else if (endReason == kEndReasonLose) {
+        message = @"You lose!";
+    }
+    
+    CCLabelBMFont *label;
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        label = [CCLabelBMFont labelWithString:message fntFile:@"Arial-hd.fnt"];
+    } else {
+        label = [CCLabelBMFont labelWithString:message fntFile:@"Arial.fnt"];
+    }
+    label.scale = 0.1;
+    label.position = ccp(winSize.width/2, winSize.height * 0.6);
+    [self addChild:label];
+    
+    CCLabelBMFont *restartLabel;
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        restartLabel = [CCLabelBMFont labelWithString:@"Restart" fntFile:@"Arial-hd.fnt"];    
+    } else {
+        restartLabel = [CCLabelBMFont labelWithString:@"Restart" fntFile:@"Arial.fnt"];    
+    }
+    
+    CCMenuItemLabel *restartItem = [CCMenuItemLabel itemWithLabel:restartLabel target:self selector:@selector(restartTapped:)];
+    restartItem.scale = 0.1;
+    restartItem.position = ccp(winSize.width/2, winSize.height * 0.4);
+    
+    CCMenu *menu = [CCMenu menuWithItems:restartItem, nil];
+    menu.position = CGPointZero;
+    [self addChild:menu];
+    
+    [restartItem runAction:[CCScaleTo actionWithDuration:0.5 scale:1.0]];
+    [label runAction:[CCScaleTo actionWithDuration:0.5 scale:1.0]];
+    
 }
 
 // on "dealloc" you need to release all your retained objects
